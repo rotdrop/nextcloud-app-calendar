f7d1637bebbafce7f39c04a55f9b00fe16a21481
Claus-Justus Heine <himself@claus-justus-heine.de>
2024-05-15 16:38:02 +0200

    Augment the category menu by system tags and already used categories.
    This commit add all available "collaborative tags" and all already used
    categories into option groups of the tags-menu of the side-bar editor.
    
    Determining the set of already used categories is a little bit ugly: it
    used the oc_calendarobject_props table which might be considered
    "internal". However, this is the Nextcloud calendar app which only talks
    to the Nextcloud calendar server. So using this "internal ingredient"
    might be acceptable.
    
    This commit addresses and is a related to a couple of open issues:
    
    nextcloud/calendar#3735 Calendar Categories: Propose Categories already used
    
    - this should be fixed by this commit
    
    nextcloud/calendar#1644 Add own categories, delete default ones
    
    - this is partly fixed in the sense that collaboritive tags are now also
      proposed as calendar categories.
    - still default categories cannot be deleted
    - however, using option groups one at least has some sort of overview
      about the origin of the proposed category
    
    nextcloud/server#29950 Save VEVENT CATEGORIES as vcategory
    
    - this issue is totally "ignored" by this commit as the proposed
      solution there is not needed (the categories are already there in the
      oc_calendarobject_props table)
    - that would have to be discussed there: but my impression that the
      tables and classed mentioned there are obsolete and no longer used.

--- src/views/EditSimple.vue
+++ src/views/EditSimple.vue
@@ -224,7 +224,7 @@
 							class="property-categories"
 							:colored-options="true"
 							:is-read-only="isReadOnly"
-							:prop-model="rfcProps.categories"
+							:prop-model="categoryOptions"
 							:value="categories"
 							@add-single-value="addCategory"
 							@remove-single-value="removeCategory" />
