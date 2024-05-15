3224c7cd62c19836840e6b38bb1668a62136fb62
Claus-Justus Heine <himself@claus-justus-heine.de>
2024-05-15 13:09:55 +0200

    PHP: Provide system tags and custom categories as initial state data.

--- lib/Service/CalendarInitialStateService.php
+++ lib/Service/CalendarInitialStateService.php
@@ -9,6 +9,7 @@ namespace OCA\Calendar\Service;
 
 use OC\App\CompareVersion;
 use OCA\Calendar\Service\Appointments\AppointmentConfigService;
+use OCA\Calendar\Service\CategoriesService;
 use OCP\App\IAppManager;
 use OCP\AppFramework\Services\IInitialState;
 use OCP\Calendar\Resource\IManager as IResourceManager;
@@ -24,6 +25,7 @@ class CalendarInitialStateService {
 		private IAppManager $appManager,
 		private IConfig $config,
 		private AppointmentConfigService $appointmentConfigService,
+		private CategoriesService $categoriesService,
 		private CompareVersion $compareVersion,
 		private ?string $userId,
 		private IResourceManager $resourceManager,
@@ -105,6 +107,7 @@ class CalendarInitialStateService {
 			'resource_booking_enabled',
 			$enableResourceBooking,
 		);
+		$this->initialStateService->provideInitialState('categories', $this->categoriesService->getCategories());
 	}
 
 	/**
