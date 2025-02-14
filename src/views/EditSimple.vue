f5f6d3a8d33bff5a942cbf0b30c49a802c346cf6
Claus-Justus Heine <himself@claus-justus-heine.de>
2025-02-14 17:15:51 +0100

    EditSimple: disable focus trap while popover is empty.
    Fixes
    
    Error in v-on handler (Promise/async): "Error: Your focus-trap must have at least one container with at least one tabbable node in it at all times"

--- src/views/EditSimple.vue
+++ src/views/EditSimple.vue
@@ -20,6 +20,7 @@
 			:auto-hide="false"
 			:placement="placement"
 			:boundary="boundaryElement"
+			:focus-trap="enableFocusTrap"
 			popover-base-class="event-popover"
 			:triggers="[]">
 			<template #trigger="{ attrs }">
@@ -300,6 +301,10 @@ export default {
 			'widgetEventDetails',
 			'widgetRef',
 		]),
+
+		enableFocusTrap() {
+			return !this.isLoading || this.isSaving
+		},
 
 		showPopover() {
 			return this.isVisible || this.widgetEventDetailsOpen
