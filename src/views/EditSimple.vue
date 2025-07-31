07f6005872a7c90dca450213a8386bbbee7a45e6
Claus-Justus Heine <himself@claus-justus-heine.de>
2025-07-31 23:00:11 +0200

    Fix unscrollable simple editor in contexts with very narrow height.

--- src/views/EditSimple.vue
+++ src/views/EditSimple.vue
@@ -502,7 +502,7 @@ export default {
 .event-popover__inner {
 	width: unset !important;
 	min-width: 500px !important;
-	max-height: 100vh !important;
+	max-height: calc(100vh - 3 * var(--default-grid-baseline) - 50px) !important;
 	overflow-y: auto !important;
 }
 
