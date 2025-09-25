95252c6c188c18f8134dc7bd2ccfa7a608635824
Claus-Justus Heine <himself@claus-justus-heine.de>
2025-09-25 18:47:59 +0200

    For shared writable calendars allow anyone with write access to change the organizer.
    The use case are long-lasting recurring events (for years ...) where one
    cannot assume that the organizer is always available to do the edits.
    And anyhow: shared writable means writable.

--- src/views/EditSimple.vue
+++ src/views/EditSimple.vue
@@ -207,7 +207,7 @@
 							:hideButtons="true"
 							:hideErrors="true"
 							:showHeader="true"
-							:isReadOnly="isReadOnlyOrViewing || isViewedByOrganizer === false"
+							:isReadOnly="isReadOnlyOrViewing/* || isViewedByOrganizer === false */"
 							:isSharedWithMe="isSharedWithMe"
 							:calendar="selectedCalendar"
 							:calendarObjectInstance="calendarObjectInstance" />
